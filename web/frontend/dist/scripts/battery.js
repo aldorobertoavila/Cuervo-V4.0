c8158529fc0aae17c88bc410a0260ae2348df380
Aldo Avila <45251738+aldorobertoavila@users.noreply.github.com>
2024-02-09 02:29:56 -0800

    feat: add battery card

--- web/frontend/dist/scripts/battery.js
+++ web/frontend/dist/scripts/battery.js
@@ -1,19 +1,53 @@
 setInterval(() => {
-    let levelElement = document.querySelector(".battery__level");
+    let batteryLevelElement = document.querySelector(".battery__level");
+    let numberElement = document.querySelector('.card__number--value');
+
     let randomLevel = Math.floor(Math.random() * 100);
 
-    levelElement.style.height = randomLevel + "%";
+    batteryLevelElement.style.height = randomLevel + "%";
+    numberElement.textContent = randomLevel;
 
     if (randomLevel <= 10) {
-        levelElement.className = "battery__level battery__level--fully_discharged";
+        batteryLevelElement.className = "battery__level battery__level--fully_discharged";
     } else if (randomLevel <= 30) {
-        levelElement.className = "battery__level battery__level--almost_discharged";
+        batteryLevelElement.className = "battery__level battery__level--almost_discharged";
     } else if (randomLevel <= 50) {
-        levelElement.className = "battery__level battery__level--mostly_charged";
+        batteryLevelElement.className = "battery__level battery__level--mostly_charged";
     } else if (randomLevel <= 75) {
-        levelElement.className = "battery__level battery__level--almost_charged";
+        batteryLevelElement.className = "battery__level battery__level--almost_charged";
     } else {
-        levelElement.className = "battery__level battery__level--fully_charged";
+        batteryLevelElement.className = "battery__level battery__level--fully_charged";
     }
 
-}, 1000);
+    let lastUpdateTimeElement = document.querySelector(".card__date-time");
+
+    let currentTime = new Date();
+
+    if (lastUpdateTimeElement.hasAttribute("datetime")) {
+        let lastUpdateTimeString = lastUpdateTimeElement.getAttribute('datetime');
+        let lastUpdateTime = new Date(lastUpdateTimeString);
+
+        lastUpdateTimeElement.textContent = formatTimeDifference(currentTime, lastUpdateTime)
+    } else {
+        lastUpdateTimeElement.textContent = formatTimeDifference(currentTime, currentTime)
+    }
+
+    lastUpdateTimeElement.setAttribute('datetime', currentTime.toISOString());
+
+}, 1500);
+
+function formatTimeDifference(currentTime, lastUpdateTime) {
+    let differenceInSeconds = Math.floor((currentTime - lastUpdateTime) / 1000);
+
+    let hours = Math.floor(differenceInSeconds / 3600);
+    let minutes = Math.floor((differenceInSeconds % 3600) / 60);
+    let seconds = differenceInSeconds % 60;
+
+    if (hours > 0) {
+        return hours + "h";
+    } else if (minutes > 0) {
+        return minutes + "m";
+    }
+
+    return seconds + "s";
+}
