47e3c148f03e0d85ca7780bcf73354ba049db1a2
Aldo Avila <45251738+aldorobertoavila@users.noreply.github.com>
2024-02-12 01:49:43 -0800

    feat: add multi-line chart

--- web/frontend/dist/scripts/highcharts/linechart.js
+++ web/frontend/dist/scripts/highcharts/linechart.js
@@ -1,6 +1,6 @@
 function randomGaussian(mean, stdDev) {
-    let theta = Math.cos(2.0 * Math.PI * Math.random());
-    let rho = Math.sqrt(-2.0 * Math.log(Math.random()))
+    const theta = Math.cos(2.0 * Math.PI * Math.random());
+    const rho = Math.sqrt(-2.0 * Math.log(Math.random()))
 
     return theta * rho * stdDev + mean;
 }
@@ -12,196 +12,341 @@ function calculateAverage(numberArray) {
     return parseFloat(average.toFixed(2));
 }
 
-function generateTemperature(min, max, mean, stdDev) {
-    const temperature = Math.max(min, Math.min(max, randomGaussian(mean, stdDev)));
+function generateRandomGaussian(min, max, mean, stdDev) {
+    const number = Math.max(min, Math.min(max, randomGaussian(mean, stdDev)));
 
-    return parseFloat(temperature.toFixed(2));
+    return parseFloat(number.toFixed(2));
 }
 
 function generateTemperatures() {
-    const rawTemperatures = Array.from({ length: 4 }, () => generateTemperature(20, 40, 30, 2));
+    const rawTemperatures = Array.from({ length: 4 }, () => generateRandomGaussian(20, 40, 30, 2));
 
     const minTemperature = Math.min(...rawTemperatures);
     const maxTemperature = Math.max(...rawTemperatures);
     const averageTemperature = calculateAverage(rawTemperatures);
 
-    return [averageTemperature, minTemperature, maxTemperature]
+    return [rawTemperatures, averageTemperature, minTemperature, maxTemperature]
+}
+
+function createDefaultLineChartSettings(chartElement) {
+    return {
+        chart: {
+            renderTo: chartElement,
+            zooming: {
+                mouseWheel: {
+                    enabled: false,
+                }
+            },
+            alignTicks: false
+        },
+        title: {
+            text: null
+        },
+        accessibility: {
+            enabled: false
+        },
+        time: {
+            useUTC: false
+        },
+        exporting: {
+            buttons: {
+                contextButton: {
+                    menuItems: [
+                        'viewFullscreen',
+                        'separator',
+                        'downloadPNG',
+                        'downloadJPEG',
+                        'downloadPDF',
+                        'downloadSVG',
+                        'separator',
+                        'downloadCSV'
+                    ]
+                }
+            }
+        },
+        yAxis: [],
+        xAxis: {
+            type: 'datetime',
+            labels: {
+                style: {
+                    fontSize: '12px',
+                }
+            }
+        },
+        plotOptions: {
+            series: {
+                findNearestPointBy: 'xy',
+                stickyTracking: false,
+                cropThreshold: 1,
+                dataGrouping: {
+                    enabled: true,
+                    units: [
+                        ['minute', [1, 5, 10, 15, 30, 45]],
+                        ['hour', [1, 6, 12]],
+                        ['day', [1]]
+                    ]
+                }
+            }
+        },
+        tooltip: {
+            shared: true,
+            valueDecimals: 2,
+            formatter: function () {
+                const datetime = Highcharts.dateFormat('%b %e, %Y %l:%M:%S %p UTC', this.x);
+
+                let tooltipContent = `<span style="font-size: 12px">${datetime}</span><br>`;
+
+                this.points.forEach((point) => {
+                    const valueSuffix = point.series.options.valueSuffix;
+
+                    if (point.series.type === 'line') {
+                        tooltipContent += `<span style="color: ${point.color}">\u25CF</span> ${point.series.name}: <b>${point.y.toFixed(2)}${valueSuffix}</b><br>`;
+                    } else if (point.series.type === 'arearange') {
+                        tooltipContent += `<span style="color: ${point.color}">\u25CF</span> ${point.series.name}: <b>${point.point.low.toFixed(2)}${valueSuffix}</b> - <b>${point.point.high.toFixed(2)}${valueSuffix}</b><br>`;
+                    }
+                });
+
+                return tooltipContent;
+            },
+        },
+        rangeSelector: {
+            buttons: [{
+                count: 1,
+                type: 'minute',
+                text: '1m'
+            },
+            {
+                count: 5,
+                type: 'minute',
+                text: '5m'
+            },
+            {
+                count: 15,
+                type: 'minute',
+                text: '15m'
+            },
+            {
+                count: 30,
+                type: 'minute',
+                text: '30m'
+            },
+            {
+                count: 1,
+                type: 'hour',
+                text: '1h'
+            },
+            {
+                type: 'all',
+                text: 'All'
+            }],
+            inputEnabled: true,
+            selected: 0
+        },
+        series: []
+    };
+}
+
+function createMultiLineChart(chartElement, initialData) {
+    const defaultSettings = createDefaultLineChartSettings(chartElement);
+
+    const settings = Object.assign({}, defaultSettings, {
+        yAxis: [
+            {
+                title: {
+                    text: 'Voltage'
+                },
+                labels: {
+                    format: '{value}V'
+                },
+                opposite: false,
+                axisPosition: 'left',
+                height: '33%',
+                offset: 0
+            },
+            {
+                title: {
+                    text: 'Current'
+                },
+                labels: {
+                    format: '{value}A'
+                },
+                opposite: false,
+                axisPosition: 'left',
+                top: '33%',
+                height: '33%',
+                offset: 0
+            },
+            {
+                title: {
+                    text: 'Power'
+                },
+                labels: {
+                    format: '{value}W'
+                },
+                opposite: false,
+                axisPosition: 'left',
+                top: '66%',
+                height: '33%',
+                offset: 0
+            }
+        ],
+        series: [
+            {
+                name: 'Voltage',
+                marker: {
+                    symbol: 'circle',
+                    fillColor: 'white',
+                    lineWidth: 2,
+                    lineColor: null
+                },
+                data: initialData.averageSeries,
+                yAxis: 0,
+                valueSuffix: "V"
+            },
+            {
+                name: 'Current',
+                marker: {
+                    symbol: 'circle',
+                    fillColor: 'white',
+                    lineWidth: 2,
+                    lineColor: null
+                },
+                data: initialData.averageSeries,
+                yAxis: 1,
+                valueSuffix: "A"
+            },
+            {
+                name: 'Power',
+                marker: {
+                    symbol: 'circle',
+                    fillColor: 'white',
+                    lineWidth: 2,
+                    lineColor: null
+                },
+                data: initialData.averageSeries,
+                yAxis: 2,
+                valueSuffix: "W"
+            }
+        ]
+    });
+
+    return Highcharts.stockChart(settings);
+}
+
+function createLineChart(chartElement, initialData) {
+    const defaultSettings = createDefaultLineChartSettings(chartElement);
+
+    const settings = Object.assign({}, defaultSettings, {
+        yAxis: {
+            opposite: false,
+            axisPosition: 'left'
+        },
+        series: [
+            {
+                name: 'Average Temperature',
+                zIndex: 1,
+                marker: {
+                    fillColor: 'white',
+                    lineWidth: 2,
+                    lineColor: Highcharts.getOptions().colors[0]
+                },
+                data: initialData.averageSeries,
+                valueSuffix: "°C"
+            },
+            {
+                name: 'Temperature Range',
+                type: 'arearange',
+                lineWidth: 0,
+                color: Highcharts.getOptions().colors[0],
+                fillOpacity: 0.3,
+                zIndex: 0,
+                marker: {
+                    fillColor: 'white',
+                    lineWidth: 2,
+                    lineColor: Highcharts.getOptions().colors[0]
+                },
+                data: initialData.rangeSeries,
+                valueSuffix: "°C"
+            }
+        ]
+    });
+
+    return Highcharts.stockChart(settings);
 }
 
 function generateInitialData() {
-    const series = { averageSeries: [], rangeSeries: [] };
     const time = new Date().getTime();
 
+    const series = { battery: { average: [], range: [], raw: [] } };
+
     for (let i = -3600; i < 0; i += 1) {
-        const [averageTemperature, minTemperature, maxTemperature] = generateTemperatures();
         const currentTime = time + i * 1000;
 
-        series.averageSeries.push([currentTime, averageTemperature]);
-        series.rangeSeries.push([currentTime, minTemperature, maxTemperature]);
+        const [rawTemperatures, averageTemperature, minTemperature, maxTemperature] = generateTemperatures();
+
+        series.battery.average.push([currentTime, averageTemperature]);
+        series.battery.range.push([currentTime, minTemperature, maxTemperature]);
+        series.battery.raw.push([currentTime, rawTemperatures]);
     }
 
-    return series;
+    return {
+        battery: {
+            current: {
+
+            },
+            power: 0, // MPPT
+            temperature: {
+                average: series.battery.average,
+                range: series.battery.range,
+                raw: series.battery.raw // Thermistors
+            },
+            voltage: 0, // MPPT
+        },
+        pv: {
+            current: 0, // MPPT
+            power: 0, // MPPT
+            voltage: 0 // MPPT
+        },
+        motor: {
+            speed: 0, // Hall effect sensor
+            temperature: 0 // Thermistor
+        }
+    };
 }
 
 const initialData = generateInitialData();
 
-const chartElement = document.querySelector('.line-chart--pv-current');
+const batteryLineChartElement = document.querySelector('.chart-block__line-chart--battery');
+const motorLineChartElement = document.querySelector('.chart-block__line-chart--motor');
+const pvLineChartElement = document.querySelector('.chart-block__line-chart--pv');
 
-const chart = Highcharts.stockChart({
-    chart: {
-        renderTo: chartElement,
-        zooming: {
-            mouseWheel: {
-                enabled: false,
-            }
-        },
-        alignTicks: false
-    },
-    title: {
-        text: 'Live random data'
-    },
-    accessibility: {
-        enabled: false
-    },
-    time: {
-        useUTC: false
-    },
-    exporting: {
-        buttons: {
-            contextButton: {
-                menuItems: [
-                    'viewFullscreen',
-                    'separator',
-                    'downloadPNG',
-                    'downloadJPEG',
-                    'downloadPDF',
-                    'downloadSVG',
-                    'separator',
-                    'downloadCSV'
-                ]
-            }
-        }
-    },
-    yAxis: {
-        opposite: false,
-        axisPosition: 'left'
-    },
-    xAxis: {
-        type: 'datetime',
-        labels: {
-            style: {
-                fontSize: '10px',
-            }
-        }
-    },
-    plotOptions: {
-        series: {
-            findNearestPointBy: 'xy',
-            stickyTracking: false,
-            cropThreshold: 1,
-            dataGrouping: {
-                enabled: true,
-                units: [
-                    ['minute', [1, 5, 10, 15, 30, 45]],
-                    ['hour', [1, 6, 12]],
-                    ['day', [1]]
-                ]
-            }
-        }
-    },
-    tooltip: {
-        shared: true,
-        valueDecimals: 2,
-        valueSuffix: '°C',
-        formatter: function () {
-            let datetime = Highcharts.dateFormat('%b %e, %Y %l:%M:%S %p UTC', this.x);
-
-            let tooltipContent = `<span style="font-size: 12px">${datetime}</span><br>`;
-
-            this.points.forEach((point) => {
-                if (point.series.type === 'line') {
-                    tooltipContent += `<span style="color: ${point.color}">\u25CF</span> ${point.series.name}: <b>${point.y.toFixed(2)}${this.series.tooltipOptions.valueSuffix}</b><br>`; // Limiting to 2 decimals
-                } else if (point.series.type === 'arearange') {
-                    tooltipContent += `<span style="color: ${point.color}">\u25CF</span> ${point.series.name}: <b>${point.point.low.toFixed(2)}${this.series.tooltipOptions.valueSuffix}</b> - <b>${point.point.high.toFixed(2)}${this.series.tooltipOptions.valueSuffix}</b><br>`; // Limiting to 2 decimals
-                }
-            });
-
-            return tooltipContent;
-        },
-    },
-    rangeSelector: {
-        buttons: [{
-            count: 1,
-            type: 'minute',
-            text: '1m'
-        },
-        {
-            count: 5,
-            type: 'minute',
-            text: '5m'
-        },
-        {
-            count: 15,
-            type: 'minute',
-            text: '15m'
-        },
-        {
-            count: 30,
-            type: 'minute',
-            text: '30m'
-        },
-        {
-            count: 1,
-            type: 'hour',
-            text: '1h'
-        },
-        {
-            type: 'all',
-            text: 'All'
-        }],
-        inputEnabled: true,
-        selected: 0
-    },
-    series: [
-        {
-            name: 'Average Temperature',
-            zIndex: 1,
-            marker: {
-                fillColor: 'white',
-                lineWidth: 2,
-                lineColor: Highcharts.getOptions().colors[0]
-            },
-            data: initialData.averageSeries
-        },
-        {
-            name: 'Temperature Range',
-            type: 'arearange',
-            lineWidth: 0,
-            color: Highcharts.getOptions().colors[0],
-            fillOpacity: 0.3,
-            zIndex: 0,
-            marker: {
-                fillColor: 'white',
-                lineWidth: 2,
-                lineColor: Highcharts.getOptions().colors[0]
-            },
-            data: initialData.rangeSeries
-        }
-    ]
-});
+const batteryLineChart = createMultiLineChart(batteryLineChartElement, { averageSeries: initialData.battery.temperature.average, rangeSeries: initialData.battery.temperature.range });
+const motorLineChart = createLineChart(motorLineChartElement, { averageSeries: initialData.battery.temperature.average, rangeSeries: initialData.battery.temperature.range });
+const pvLineChart = createLineChart(pvLineChartElement, { averageSeries: initialData.battery.temperature.average, rangeSeries: initialData.battery.temperature.range });
 
 setInterval(function () {
-    const [averageTemperature, minTemperature, maxTemperature] = generateTemperatures();
     const time = new Date().getTime();
 
-    const extremes = chart.xAxis[0].getExtremes();
+    const [rawTemperatures, averageTemperature, minTemperature, maxTemperature] = generateTemperatures();
+
+    const extremes = batteryLineChart.xAxis[0].getExtremes();
     const duration = extremes.max - extremes.min;
     const redraw = duration <= 15 * 60 * 1000;
 
-    chart.series[1].addPoint([time, minTemperature, maxTemperature], false, false);
-    chart.series[0].addPoint([time, averageTemperature], false, false);
+    batteryLineChart.series[0].addPoint([time, averageTemperature], false, false);
+    batteryLineChart.series[1].addPoint([time, averageTemperature], false, false);
+    batteryLineChart.series[2].addPoint([time, averageTemperature], false, false);
+
+    motorLineChart.series[0].addPoint([time, averageTemperature], false, false);
+    motorLineChart.series[1].addPoint([time, minTemperature, maxTemperature], false, false);
+
+    pvLineChart.series[0].addPoint([time, averageTemperature], false, false);
+    pvLineChart.series[1].addPoint([time, minTemperature, maxTemperature], false, false);
 
     if (redraw) {
-        chart.redraw();
+        batteryLineChart.redraw();
+        motorLineChart.redraw();
+        pvLineChart.redraw();
     }
+
 }, 1100);
