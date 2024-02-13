a752955b30751a2fcdd34a17eb3450f4babf43d6
Aldo Avila <45251738+aldorobertoavila@users.noreply.github.com>
2024-02-13 01:24:25 -0800

    feat!: add navbar and icons

--- web/frontend/dist/scripts/cards.js
+++ web/frontend/dist/scripts/cards.js
@@ -1,24 +1,4 @@
-setInterval(() => {
-    let batteryLevelElement = document.querySelector(".battery__level");
-    let numberElement = document.querySelector('.card__number--value');
-
-    let randomLevel = Math.floor(Math.random() * 100);
-
-    batteryLevelElement.style.height = randomLevel + "%";
-    numberElement.textContent = randomLevel;
-
-    if (randomLevel <= 10) {
-        batteryLevelElement.className = "battery__level battery__level--fully_discharged";
-    } else if (randomLevel <= 30) {
-        batteryLevelElement.className = "battery__level battery__level--almost_discharged";
-    } else if (randomLevel <= 50) {
-        batteryLevelElement.className = "battery__level battery__level--mostly_charged";
-    } else if (randomLevel <= 75) {
-        batteryLevelElement.className = "battery__level battery__level--almost_charged";
-    } else {
-        batteryLevelElement.className = "battery__level battery__level--fully_charged";
-    }
-
+function updateDaytime() {
     let lastUpdateTimeElement = document.querySelector(".card__date-time");
 
     let currentTime = new Date();
@@ -33,8 +13,7 @@ setInterval(() => {
     }
 
     lastUpdateTimeElement.setAttribute('datetime', currentTime.toISOString());
-
-}, 1500);
+}
 
 function formatTimeDifference(currentTime, lastUpdateTime) {
     let differenceInSeconds = Math.floor((currentTime - lastUpdateTime) / 1000);
