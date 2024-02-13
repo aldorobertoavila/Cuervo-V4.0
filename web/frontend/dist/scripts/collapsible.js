fb2ec156052b7e68b8ff97a2144b003ff78b441a
Aldo Avila <45251738+aldorobertoavila@users.noreply.github.com>
2024-02-12 19:47:53 -0800

    feat: add collapsible content sections

---
+++ web/frontend/dist/scripts/collapsible.js
@@ -0,0 +1,27 @@
+const collapsibles = document.querySelectorAll('.collapsible-content-section');
+
+collapsibles.forEach(collapsible => {
+  const header = collapsible.querySelector('.collapsible-content-section__header');
+  const content = collapsible.querySelector('.collapsible-content-section__body');
+  // const footer = collapsible.querySelector('.collapsible-content-section__footer');
+
+  const toggleContent = () => {
+    collapsible.classList.toggle('active');
+
+    if (collapsible.classList.contains('active')) {
+      content.style.maxHeight = content.scrollHeight + 'px';
+      content.style.opacity = '1';
+    } else {
+      content.style.maxHeight = '0';
+      content.style.opacity = '0';
+    }
+  };
+
+  header.addEventListener('click', toggleContent);
+  // footer.addEventListener('click', toggleContent);
+
+  if (collapsible.classList.contains('active')) {
+    content.style.maxHeight = content.scrollHeight + 'px';
+    content.style.opacity = '1';
+  }
+});
